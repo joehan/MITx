b76dc58e7d9888f34c856edefc29ee073cea669a
Joe Hanley <jhanley@mit.edu>
2013-06-19 10:34:23 -0400

    svg graphs now

--- day8/visual.js
+++ day8/visual.js
@@ -3,8 +3,20 @@ var dataList = [0,4,8,8,15,16,23,42];
 var x_scale = d3.scale.linear()
                 .domain([0,d3.max(dataList)]).range(['0%', '100%']);
 
-var chart = d3.select(".chart-container").append("div").attr("class","chart");
+var chart_height = 140;
+var y_scale = d3.scale.ordinal().domain(d3.keys(dataList)).rangeBands([0,chart_height]);
 
-chart.selectAll("div").data(dataList).enter().append("div")
-.style("width", x_scale)
-.text(function(d) {return d});
+var chart = d3.select(".chart-container").append("svg").attr("class","chart");
+
+chart.selectAll("rect").data(dataList).enter().append("rect")
+    .attr('y', function(d,i){return y_scale(i)})
+    .attr("width", x_scale)
+    .attr('height', 20);
+
+chart.selectAll('text').data(dataList).enter().append('text')
+    .attr('x', x_scale)
+    .attr('y', function(d,i){return y_scale(i)+y_scale.rangeBand()/2})
+    .attr('dy', '.035em')
+    .attr('dx', -3)
+    .attr('text-anchor','end')
+    .text(function(d){return d})
