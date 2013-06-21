291b978bae7844e4f7e040dfbfe0a99f3cabb874
Joe Hanley <jhanley@mit.edu>
2013-06-21 13:29:09 -0400

    Working well. Now trying to make more flexible

--- Knapsack/Knapsack.js
+++ Knapsack/Knapsack.js
@@ -1,7 +1,5 @@
 /*
 TODO:
-Make value box update on each movement
-Make a weight limit
     -change values so that the problem has an answer
 Enforce a weight limit
     -Make value text red, inform user that they are over limit, suggest they put some stuff back
@@ -11,7 +9,11 @@ css styling
 
 */
 var populated= false;
+var weightLimit= 200;
 
+var valueList = [{'price':110, 'weight':100}, {'price':35, 'weight':25}, {'price':200, 'weight':200} ,{'price':5, 'weight':5}, {'price':130, 'weight':105}, {'price':100, 'weight':70}]
+
+var winningPrice= 265;
 
 var populateValues = (function(div, priceOutput, weightOutput){
     var totalPrice= 0
@@ -31,7 +33,11 @@ var populateValues = (function(div, priceOutput, weightOutput){
     }
     populated=true
     priceOutput.text('Total Price: $'+totalPrice)
-    weightOutput.text('Total weight:' +totalWeight+'lbs')
+    weightOutput.text('Weight Left: ' +(weightLimit-totalWeight)+'lbs')
+    if (div.hasClass('bag')){
+        limitChecker(weightLimit, totalWeight)
+        winChecker(totalWeight,totalPrice,weightLimit,winningPrice)
+    }
 });
 
 
@@ -43,7 +49,7 @@ var switchSide = (function (item){
     if (loc == 'house'){
         var target = '.bag'+pos
         var info = item.parent().text()
-        item.parent().empty()
+        item.parent().attr('data-on', false).empty()
         $(target).append(item).append(info)
         .attr('data-on',true)
         console.log('bag'+$(target).attr('data-on'))
@@ -51,17 +57,46 @@ var switchSide = (function (item){
     else if (loc == 'bag'){
         var target = '.house'+pos
         var info = item.parent().text()
-        item.parent().empty()
+        item.parent().attr('data-on', false).empty()
         $(target).append(item).append(info)
         .attr('data-on', true)
         console.log('house'+$(target).attr('data-on'))
     }
-    item.parent().attr('data-on',false)
     console.log('other'+item.parent().attr('data-on'))
     populateValues($('.house'), $('.housePrice'), $('.houseWeight'))
     populateValues($('.bag'), $('.bagPrice'), $('.bagWeight'))
 })
 
+
+var limitChecker =(function(limit, weight){
+    if (weight>limit){
+        $('.bagPrice').addClass('overLimit')
+        $('.bagWeight').addClass('overLimit')
+        $('.warning').removeClass('notVisible')
+    }
+    else{
+        $('.bagPrice').removeClass('overLimit')
+        $('.bagWeight').removeClass('overLimit')
+        $('.warning').addClass('notVisible')
+        
+    }
+    
+})
+
+var winChecker = (function(weight, price, weightLimit, winPrice){
+    if ((weight<=weightLimit)&&(price==winPrice)){
+        $('.warning').addClass('winner').removeClass('notVisible').text('Congratulations! You win!')
+        $('.bagPrice').addClass('winner')
+        $('.bagWeight').addClass('winner')
+    }
+    else{
+        $('.bagPrice').removeClass('winner')
+        $('.bagWeight').removeClass('winner')
+        $('.warning').removeClass('winner').text('Too heavy! Try putting some stuff back.')
+    }
+})
+
 $(document).ready(function(){  
     populateValues($('.house'), $('.housePrice'), $('.houseWeight'))
+    populateValues($('.bag'), $('.bagPrice'),$('.bagWeight'))
 })
